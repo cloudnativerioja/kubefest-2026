c77522efe8b9184a77d81dda7ca853c1d38e3230
statickidz <statickidz@gmail.com>
2025-12-23 14:05:58 +0100

    feat: add image passthrough service to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  image: {
+    // Use passthrough service to avoid Sharp requirement in CI
+    service: { entry: 'astro/assets/services/noop' },
+  },
   server: {
     port: 4000,
     host: 'localhost',
