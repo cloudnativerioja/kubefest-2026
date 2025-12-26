bd156a2663fcc1981965f3a5c8c2b833222f42c8
statickidz <statickidz@gmail.com>
2025-12-26 13:27:30 +0100

    feat: add compress and partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+
 import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+import playformCompress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), partytown(), playformCompress()],
   image: {
     // Use passthrough service to avoid Sharp requirement in CI
     service: { entry: 'astro/assets/services/noop' },
@@ -13,4 +16,3 @@ export default defineConfig({
     host: 'localhost',
   },
 });
-
